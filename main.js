e2fcf84a07cb2a47ea361372a8897c541e5d72ca
clement tribouillard <clem.trib@free.fr>
2018-12-13 13:31:02 -0800

    step 03 - Conditional Rendering

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var app = new Vue({
     product: 'Socks',
     description: 'A pair of warm fuzzy socks',
     image: 'https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
-    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    inventory: 100,
+    onSale: false
   } 
 })
