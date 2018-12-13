32154880dd972fcb1711ebccd3b0abbde75dbe05
clement tribouillard <clem.trib@free.fr>
2018-12-13 13:05:57 -0800

    step 02 - Intro to Vue

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm fuzzy socks'
+    description: 'A pair of warm fuzzy socks',
+    image: 'https://dl.dropboxusercontent.com/s/9zccs3f0pimj0wj/vmSocks-green-onWhite.jpg?dl=0',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
   } 
 })
